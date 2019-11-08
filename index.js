7948ef83e20124ed5c66c94c67966188f40109bb
vithort <vithort@gmail.com>
2019-11-08 14:33:56 -0300

    Iniciar
    Inicializar:
    > npm init -y
    
    Instalar dependências:
    > npm i puppeteer

---
+++ index.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer')
+
+const tirarScreenshot = async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://devpleno.com')
+    //await page.screenshot({ path: 'devpleno.png' })
+    await page.pdf({
+        path: 'devpleno.pdf'
+        , format: 'A4'
+    })
+    await page.close()
+}
+
+//tirarScreenshot()
+
+const getInfo = async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://devpleno.com')
+
+    const info = await page.evaluate(() => {
+        return {
+            title: document.title
+            ,w: document.documentElement.clientWidth
+        }
+    })
+    console.log(info)
+
+    await page.close()
+}
+
+getInfo()
